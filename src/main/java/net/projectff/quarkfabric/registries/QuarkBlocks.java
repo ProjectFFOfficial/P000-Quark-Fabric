f527cacf7110bf3ed277b85eaf27bd1ab69a2f5e
StonksMan001 <toth.abel.2007@gmail.com>
2024-07-28 22:20:47 +0200

    Deleted previous registry classes

--- src/main/java/net/projectff/quarkfabric/registries/QuarkBlocks.java
+++ src/main/java/net/projectff/quarkfabric/registries/QuarkBlocks.java
@@ -12,41 +12,15 @@ import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.projectff.quarkfabric.base.Quark;
+import net.projectff.quarkfabric.config.QuarkConfigProvider;
+import net.projectff.quarkfabric.config.QuarkConfigs;
 import net.projectff.quarkfabric.content.automation.block.ChuteBlock;
 import net.projectff.quarkfabric.content.automation.block.EnderWatcherBlock;
 import net.projectff.quarkfabric.content.automation.block.GravisandBlock;
 
 public class QuarkBlocks {
-    public static final Block GRAVISAND = registerBlockWithItem("gravisand",
-            new GravisandBlock(FabricBlockSettings
-                    .copy(Blocks.SAND)));
-    public static final Block CHUTE = registerBlockWithItem("chute",
-            new ChuteBlock(FabricBlockSettings
-                    .of().mapColor(MapColor.OAK_TAN)
-                    .strength(2.5F)
-                    .sounds(BlockSoundGroup.WOOD)
-                    .burnable()));
-    public static final Block ENDER_WATCHER = registerBlockWithItem("ender_watcher",
-            new EnderWatcherBlock(FabricBlockSettings
-                    .create().mapColor(MapColor.GREEN)
-                    .strength(3f, 10f)
-                    .sounds(BlockSoundGroup.METAL)));
+    public static Block CHUTE;
+    public static Block ENDER_WATCHER;
+    public static Block GRAVISAND;
     public static Block GRATE;
-
-
-    private static Block registerBlockWithoutItem(String name, Block block) {
-        return Registry.register(Registries.BLOCK, new Identifier(Quark.MOD_ID, name), block);
-    }
-
-    private static Block registerBlockWithItem(String name, Block block) {
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, new Identifier(Quark.MOD_ID, name), block);
-    }
-    private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registries.ITEM, new Identifier(Quark.MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
-    }
-
-    public static void registerQuarkBlocks() {
-        Quark.LOGGER.debug("[Quark] Registering Blocks");
-    }
 }
--- src/main/java/net/projectff/quarkfabric/registries/QuarkEntities.java
+++ src/main/java/net/projectff/quarkfabric/registries/QuarkEntities.java
@@ -9,19 +9,4 @@ import net.projectff.quarkfabric.base.Quark;
 import net.projectff.quarkfabric.content.automation.entity.Gravisand;
 
 public class QuarkEntities {
-
-    public static final EntityType<Gravisand> GRAVISAND = registerEntityType("gravisand",
-            EntityType.Builder.<Gravisand>create(Gravisand::new, SpawnGroup.MISC)
-                    .setDimensions(0.98F, 0.98F)
-                    .maxTrackingRange(10)
-                    .trackingTickInterval(20) // update interval
-                    .build());
-
-    public static EntityType registerEntityType(String name, EntityType type) {
-        return Registry.register(Registries.ENTITY_TYPE, new Identifier(Quark.MOD_ID, name), type);
-    }
-
-    public static void registerQuarkEntities() {
-        Quark.LOGGER.debug("[Quark] Registering Entities");
-    }
 }
