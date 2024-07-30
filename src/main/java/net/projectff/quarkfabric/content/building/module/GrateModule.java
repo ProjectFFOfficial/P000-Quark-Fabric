cdd434272d277e09c5789d12b4516170e3d94bf5
StonksMan001 <toth.abel.2007@gmail.com>
2024-07-30 14:37:01 +0200

    new GrateModule for future use

---
+++ src/main/java/net/projectff/quarkfabric/content/building/module/GrateModule.java
@@ -0,0 +1,12 @@
+package net.projectff.quarkfabric.content.building.module;
+
+import net.minecraft.block.Block;
+import net.projectff.quarkfabric.internal_zeta.ZetaModule;
+
+public class GrateModule extends ZetaModule {
+
+    public static Block grateBlock;
+    public static final void register() {
+
+    }
+}
