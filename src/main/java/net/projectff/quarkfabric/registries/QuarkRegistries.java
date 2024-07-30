b8b01cb0c0b84c61aa780d5568c8feeddd22a14f
StonksMan001 <toth.abel.2007@gmail.com>
2024-07-30 14:34:08 +0200

    Changed rawly used EntityType and BlockEntityType classes to genericly used ones in register...() methods

--- src/main/java/net/projectff/quarkfabric/registries/QuarkRegistries.java
+++ src/main/java/net/projectff/quarkfabric/registries/QuarkRegistries.java
@@ -3,7 +3,9 @@ package net.projectff.quarkfabric.registries;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -11,24 +13,23 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.projectff.quarkfabric.base.Quark;
-import net.projectff.quarkfabric.config.QuarkConfigs;
 import net.projectff.quarkfabric.internal_zeta.ZetaBock;
 
 public class QuarkRegistries {
-    public static Block registerBlockWithoutItem(String name, Block block) {
+    public static Block registerBlock(String name, Block block) {
         return Registry.register(Registries.BLOCK, new Identifier(Quark.MOD_ID, name), block);
     }
-    public static Block registerBlockWithItem(String name, Block block) {
+    public static Block registerBlockAndItem(String name, Block block) {
         registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, new Identifier(Quark.MOD_ID, name), block);
+        return registerBlock(name, block);
     }
     public static Item registerBlockItem(String name, Block block) {
         return Registry.register(Registries.ITEM, new Identifier(Quark.MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
     }
-    public static EntityType registerEntityType(String name, EntityType type) {
+    public static <T extends Entity> EntityType registerEntityType(String name, EntityType<T> type) {
         return Registry.register(Registries.ENTITY_TYPE, new Identifier(Quark.MOD_ID, name), type);
     }
-    public static BlockEntityType registerBlockEntityType(String name, BlockEntityType type) {
+    public static <T extends BlockEntity> BlockEntityType<T> registerBlockEntityType(String name, BlockEntityType<T> type) {
         return Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(Quark.MOD_ID, name), type);
     }
     public static void addFlammableZetablock(Block block) {
